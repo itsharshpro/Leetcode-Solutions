ee84951a74a70b88a7decddcf2c5798b73c31363
itsharshpro <harshpro2004@gmail.com>
2024-09-27 16:43:57 +0530

    java

---
+++ Solution_162_Find_Peak_Element.java
@@ -0,0 +1,20 @@
+public class Solution_162_Find_Peak_Element {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,2,1};
+        System.out.println(findPeakElement(arr));
+    }
+    static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if (nums[mid]>nums[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
