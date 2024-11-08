fcb748d08bde86593515790d7ba4282a7699ffb7
itsharshpro <harshpro2004@gmail.com>
2024-11-08 21:09:36 +0530

    java

---
+++ Solution_709_To_Lower_Case.java
@@ -0,0 +1,9 @@
+public class Solution_709_To_Lower_Case {
+    public static void main(String[] args) {
+        String str = "Nice Hello";
+        System.out.println(toLowerCase(str));
+    }
+    static public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
