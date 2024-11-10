8954b864c7cc76589e2e3dc2ba2e689ce1f29599
itsharshpro <harshpro2004@gmail.com>
2024-11-10 20:44:15 +0530

    java

---
+++ Solution_2235_Add_Two_Integers.java
@@ -0,0 +1,8 @@
+public class Solution_2235_Add_Two_Integers {
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+    }
+    static public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
