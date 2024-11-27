371ba3c37a51e30c30e5cea389a270d59c599f15
itsharshpro <harshpro2004@gmail.com>
2024-11-27 23:04:42 +0530

    java

---
+++ Solution_3162_Find_the_Number_of_Good_Pairs_I.java
@@ -0,0 +1,19 @@
+public class Solution_3162_Find_the_Number_of_Good_Pairs_I {
+    public static void main(String[] args) {
+        int[] nums1 = {1,3,4};
+        int[] nums2 = {1,3,4};
+        int k = 1;
+        System.out.println(numberOfPairs(nums1, nums2, k));
+    }
+    static public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int count = 0;
+        for(int i = 0; i<nums1.length; i++){
+            for(int j = 0; j<nums2.length; j++){
+                if(nums1[i]%(nums2[j]*k)==0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
